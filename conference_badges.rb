e10a0c91a1d60209788d37557f91ec1368b3afd0
talryan <nataliamryan@gmail.com>
2020-10-22 22:14:48 -0700

    commit tree 4b78ba8c5d09dcd8c7d587edb6216abafae944a2

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+     "Hello, my name is #{name}."
+end
+
+    def batch_badge_creator(array)
+        badge_message_array = []
+        array.each do |names|
+          badge_message = badge_maker(names)
+        badge_message_array << badge_message
+      end
+      return badge_message_array
+      end
+
+      def assign_rooms(array)
+        room_assignment_array = []
+        array.each_with_index do |names, index|
+          room_number = index + 1
+          room_assignment = "Hello, #{names}! You'll be assigned to room #{room_number}!"
+          room_assignment_array << room_assignment
+        end
+        return room_assignment_array
+      end
+
+      def printer(attendees)
+        batch_badge_creator(attendees).each do |badge|
+            puts badge
+        end
+        assign_rooms(attendees).each do |assignment|
+            puts assignment
+        end
+      end
